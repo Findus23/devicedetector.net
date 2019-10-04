9065d20fa24af60bc8cdc5f56f98db7b85754d96
Lukas Winkler <git@lw1.at>
2019-10-04 12:34:49 +0200

    add count of detected devices

--- client/src/views/DetectedDevices.vue
+++ client/src/views/DetectedDevices.vue
@@ -7,25 +7,25 @@
       <h1>What Device Detector is able to detect</h1>
 
       <em>Last update: {{lastUpdated.toLocaleString()}}</em>
-      <h3>List of detected operating systems:</h3>
+      <h3>List of {{supported.operatingSystems.length}} detected operating systems:</h3>
       <p>{{ supported.operatingSystems.join(", ") }}</p>
-      <h3>List of detected browsers:</h3>
+      <h3>List of {{supported.browsers.length}} detected browsers:</h3>
       <p>{{supported.browsers.join(", ")}}</p>
-      <h3>List of detected browser engines:</h3>
+      <h3>List of {{supported.engines.length}} detected browser engines:</h3>
       <p>{{supported.engines.join(", ")}}</p>
-      <h3>List of detected libraries:</h3>
+      <h3>List of {{supported.libraries.length}} detected libraries:</h3>
       <p>{{supported.libraries.join(", ")}}</p>
-      <h3>List of detected media players:</h3>
+      <h3>List of {{supported.mediaPlayer.length}} detected media players:</h3>
       <p>{{supported.mediaPlayer.join(", ")}}</p>
-      <h3>List of detected mobile apps:</h3>
+      <h3>List of {{supported.mobileApps.length}} detected mobile apps:</h3>
       <p>{{supported.mobileApps.join(", ")}}</p>
-      <h3>List of detected PIMs (personal information manager):</h3>
+      <h3>List of {{supported.PIM.length}} detected PIMs (personal information manager):</h3>
       <p>{{supported.PIM.join(", ")}}</p>
-      <h3>List of detected feed readers:</h3>
+      <h3>List of {{supported.feedReaders.length}} detected feed readers:</h3>
       <p>{{supported.feedReaders.join(", ")}}</p>
-      <h3>List of brands with detected devices:</h3>
+      <h3>List of the {{supported.brands.length}} brands with detected devices:</h3>
       <p>{{supported.brands.join(", ")}}</p>
-      <h3>List of detected bots:</h3>
+      <h3>List of {{supported.bots.length}} detected bots:</h3>
       <p>{{supported.bots.join(", ")}}</p>
     </div>
   </div>
